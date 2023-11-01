a417bf1c02e884f45119c84bc14f7979dd144e42
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-01 12:21:52 +0100

    Modify MidLayer and GearWheel

--- src/components/layout/MidLayer/MidLayer.js
+++ src/components/layout/MidLayer/MidLayer.js
@@ -1,13 +1,14 @@
 import GearWheel from 'components/layout/GearWheel/GearWheel';
+import { Container } from 'react-bootstrap';
 import styles from './MidLayer.module.scss';
 
 const MidLayer = () => {
   return (
-    <>
-      <div className={styles.MidLayer}>
+    <div className={styles.MidLayer}>
+      <Container>
         <GearWheel />
-      </div>
-    </>
+      </Container>
+    </div>
   );
 };
 
